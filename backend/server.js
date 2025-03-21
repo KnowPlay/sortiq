da6164f927d3e0bf8a9168d0c2b00d7736b0e8fa
Sarah <xiaowei.lin@outlook.com>
2025-03-21 17:29:19 +0000

    init

---
+++ backend/server.js
@@ -0,0 +1,15 @@
+import express from 'express';
+
+const app = express();
+
+// GET Request
+app.get("/products", (req, res) => {
+    res.send("Server is ready.");
+});
+
+
+
+
+app.listen(5000, () => {
+    console.log('Server started at http://localhost:5000');
+});
