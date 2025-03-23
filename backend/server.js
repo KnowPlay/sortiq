face3757d00dc2e81249d8ff4037f76e8266039a
Sarah <xiaowei.lin@outlook.com>
2025-03-23 20:39:34 +0000

    dbi

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,7 @@ import express from "express";
 const app = express();
 
 // GET Request
-app.get("/", (req, res) => {
+app.get("/products", (req, res) => {
     res.send("Hello World");
 })
 
@@ -11,3 +11,4 @@ app.get("/", (req, res) => {
 app.listen(5000, () => {
     console.log("Server started at http://localhost:5000");
 });
+
